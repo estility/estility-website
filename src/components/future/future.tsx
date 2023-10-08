afcb8083ee6644089a742328cc861556d4ada8ab
evabanegacom <udegbue69@gmail.com>
2023-10-08 13:03:06 +0100

    worked on the UI fix

--- src/components/future/future.tsx
+++ src/components/future/future.tsx
@@ -6,7 +6,7 @@ const Future = () => {
     <div className='future-container text-center violet-background d-flex flex-column text-white sora-font align-items-center justify-center'>
         <div className='future-title'>Embrace the future today!</div>
         <div className='future-subtitle'>Experience the power of our product suite and let us guide you into the future of gated community management.</div>
-         <button>Request A demo</button>
+         <button className='violet-color future-button'>Request A demo</button>
     </div>
   )
 }
