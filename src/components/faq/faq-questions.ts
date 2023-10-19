48bfe57b793f9cf93a5855b039a6af1fb1b05f2e
evabanegacom <udegbue69@gmail.com>
2023-10-19 16:56:55 +0100

    concluded faq additions

--- src/components/faq/faq-questions.ts
+++ src/components/faq/faq-questions.ts
@@ -64,96 +64,163 @@ export const estateMgt = [
 export const estateSecurity = [
     {
         id: 1,
-        question: 'What is the Estate Security Software used for?',
-        answer: 'i dont know',
+        question: "What are the key features of Estility's software?",
+        answer: "Estility offers a software solution that encompasses both a Resident User App and an Estate Management App. The Resident User App empowers residents by enabling them to perform various tasks, including making payments for dues and utility bills, requesting access codes, ordering groceries and cooking gas from vendors, engaging with Estility's Support team, and leaving messages for estate management. On the other hand, the Estate Management App caters to estate and facility managers, providing them with tools to efficiently manage their communities. This includes functionalities such as onboarding new residents, monitoring due collections, and effectively managing various aspects of the community. Additionally, the software includes a robust security management system that facilitates the check-in and check-out process for visitors using access codes, ensuring enhanced security measures and more streamlined operations.",
     },
 
     {
         id: 2,
-        question: 'Can I access property and resident information through the software?',
-        answer: 'Yes',
+        question: 'How can I request a demo or get started with Estility?',
+        answer: 'You can click the "Schedule a Demo" link on our website or send us an email at support@estility.co to request a demonstration. (Email to be set up)',
     },
 
     {
         id: 3,
-        question: 'Does the software support different estate sizes?',
-        answer: 'YES'
+        question:"Is Estility's software easy to use?",
+        answer: "Yes, the Estate app is designed for user-friendliness. After your estate or gated community signs up, all that's required is to download the app from your respective app store, either Play Store for Android or the IOS store for Apple devices, and then sign in using your registered email address. It's a straightforward process to get started with the app."
     },
 
     {
         id: 4,
-        question: 'Can I access property and resident information through the software?',
-        answer: 'Yes',
+        question: "Is Estility's software secure and reliable?",
+        answer: "Absolutely, we take security and reliability seriously. We have implemented comprehensive measures to ensure the utmost security and protection of our residents' details. Your data privacy and safety are our top priorities.",
     },
 
     {
         id: 5,
-        question: 'Does the software support different estate sizes?',
-        answer: 'YES'
+        question: "How can I contact Estility's customer support team?",
+        answer: "Our business hours are from 8 am until we complete our last delivery of the day. You can always reach out to us via email at support@estility.co or chat with us directly through the Estility App. We're here to assist you whenever you need us."
+    }, 
+
+    {
+        id: 6,
+        question: "What is the pricing structure for Estility's services?",
+        answer: "We take pride in offering a straightforward and affordable pricing structure, ensuring ease and accessibility for our customers."
+    },
+
+    {
+        id: 7,
+        question: "How can I stay updated with Estility's latest features and updates?",
+        answer: "To stay updated with our latest news and updates, simply sign up for our newsletter. Additionally, you can follow us on our social media platforms. Find us at @estilityng on Facebook, Twitter, Instagram, and LinkedIn. We look forward to keeping you informed!"
+    },
+
+    {
+        id: 8,
+        question: "Provide information on the onboarding process for communities interested in using the software.",
+        answer: "Estate managements will be contacted and guided through the onboarding process. We're here to assist and ensure a smooth transition to using our services."
+    }, 
+
+    {
+        id: 9,
+        question: "What should I do if I encounter technical issues with the software?",
+        answer: "If you encounter any issues, please don't hesitate to contact us at support@estility.co. Simply provide us with the details of the issue, and we will work diligently to resolve it promptly. Your satisfaction is our priority."
     }
 ]
 
 export const residentApp = [
     {
         id: 1,
-        question: 'What is the Estate Resident used for?',
-        answer: 'i dont know',
+        question: 'How does the online groceries shopping service work?',
+        answer: "It's simple: just select the products you need, add them to your cart, place your order, and our dedicated Estility Rider will ensure your order is processed and delivered to your doorstep within 45 minutes. Convenience at your fingertips!",
     },
 
     {
         id: 2,
-        question: 'Can I access property and resident information through the software?',
-        answer: 'Yes',
+        question: 'What are the benefits of using Estility for cooking gas delivery?',
+        answer: 'At Estility, we take pride in offering the best prices, timely delivery, and a guarantee of both quantity and quality. Your satisfaction is our commitment.',
     },
 
     {
         id: 3,
-        question: 'Does the software support different estate sizes?',
-        answer: 'YES'
+        question: 'What types of payment methods are accepted for dues and services?',
+        answer: 'Estility offers a convenient wallet system within the Estate App. This allows for all payments and dues collections to be efficiently managed and processed within the app, providing a seamless financial experience for both residents and estate management.'
     },
 
     {
         id: 4,
-        question: 'Can I access property and resident information through the software?',
-        answer: 'Yes',
+        question: "What are the key features of Estility's software?",
+        answer: "Estility offers a software solution that encompasses both a Resident User App and an Estate Management App. The Resident User App empowers residents by enabling them to perform various tasks, including making payments for dues and utility bills, requesting access codes, ordering groceries and cooking gas from vendors, engaging with Estility's Support team, and leaving messages for estate management. On the other hand, the Estate Management App caters to estate and facility managers, providing them with tools to efficiently manage their communities. This includes functionalities such as onboarding new residents, monitoring due collections, and effectively managing various aspects of the community. Additionally, the software includes a robust security management system that facilitates the check-in and check-out process for visitors using access codes, ensuring enhanced security measures and more streamlined operations.",
     },
 
     {
         id: 5,
-        question: 'Does the software support different estate sizes?',
-        answer: 'YES'
+        question: 'How can I request a demo or get started with Estility?',
+        answer: 'You can click the "Schedule a Demo" link on our website or send us an email at support@estility.co to request a demonstration. (Email to be set up)'
+    },
+
+    {
+        id: 6,
+        question: "Is Estility's software easy to use?",
+        answer: "Yes, the Estate app is designed for user-friendliness. After your estate or gated community signs up, all that's required is to download the app from your respective app store, either Play Store for Android or the IOS store for Apple devices, and then sign in using your registered email address. It's a straightforward process to get started with the app."
+    }, 
+
+    {
+        id: 7,
+        question: "Is Estility's software secure and reliable?",
+        answer: "Absolutely, we take security and reliability seriously. We have implemented comprehensive measures to ensure the utmost security and protection of our residents' details. Your data privacy and safety are our top priorities."
+    },
+
+    {
+        id: 8,
+        question: "How can I contact Estility's customer support team?",
+        answer: "Our business hours are from 8 am until we complete our last delivery of the day. You can always reach out to us via email at support@estility.co or chat with us directly through the Estility App. We're here to assist you whenever you need us."
+    },
+
+    {
+        id: 9,
+        question: "How can I stay updated with Estility's latest features and updates?",
+        answer: "To stay updated with our latest news and updates, simply sign up for our newsletter. Additionally, you can follow us on our social media platforms. Find us at @estilityng on Facebook, Twitter, Instagram, and LinkedIn. We look forward to keeping you informed!"
+    },
+
+
+    {
+        id: 10,
+        question: "Specify the devices and operating systems supported by the software.",
+        answer: "The Estility app is accessible and available for download on both the IOS App Store and the Google Play Store, making it compatible with a wide range of devices for your convenience."
+    },
+
+    {
+        id: 11,
+        question: "What should I do if I encounter technical issues with the software?",
+        answer: "If you encounter any issues, please don't hesitate to contact us at support@estility.co. Simply provide us with the details of the issue, and we will work diligently to resolve it promptly. Your satisfaction is our priority."
     }
 ]
 
 export const partners = [
     {
         id: 1,
-        question: 'What is the Estate Partners Software used for?',
-        answer: 'i dont know',
+        question: 'How can I contact Estility for partnership or business inquiries?',
+        answer: 'Yes, we are open to collaborating with partners. If you are interested in becoming a vendor or partnering with Estility, please reach out to us by sending an email to vendor@estility.co. We look forward to exploring potential partnerships with you.',
     },
 
     {
         id: 2,
-        question: 'Can I access property and resident information through the software?',
-        answer: 'Yes',
+        question: "What are the key features of Estility's software?",
+        answer: "Estility offers a software solution that encompasses both a Resident User App and an Estate Management App. The Resident User App empowers residents by enabling them to perform various tasks, including making payments for dues and utility bills, requesting access codes, ordering groceries and cooking gas from vendors, engaging with Estility's Support team, and leaving messages for estate management. On the other hand, the Estate Management App caters to estate and facility managers, providing them with tools to efficiently manage their communities. This includes functionalities such as onboarding new residents, monitoring due collections, and effectively managing various aspects of the community. Additionally, the software includes a robust security management system that facilitates the check-in and check-out process for visitors using access codes, ensuring enhanced security measures and more streamlined operations.",
     },
 
     {
         id: 3,
-        question: 'Does the software support different estate sizes?',
-        answer: 'YES'
+        question: 'How can I request a demo or get started with Estility?',
+        answer: 'You can click the "Schedule a Demo" link on our website or send us an email at support@estility.co to request a demonstration. (Email to be set up)'
     },
 
     {
         id: 4,
-        question: 'Can I access property and resident information through the software?',
-        answer: 'Yes',
+        question: "Is Estility's software easy to use?",
+        answer: "Yes, the Estate app is designed for user-friendliness. After your estate or gated community signs up, all that's required is to download the app from your respective app store, either Play Store for Android or the IOS store for Apple devices, and then sign in using your registered email address. It's a straightforward process to get started with the app.",
     },
 
     {
         id: 5,
-        question: 'Does the software support different estate sizes?',
-        answer: 'YES'
+        question: "Is Estility's software secure and reliable?",
+        answer: "Absolutely, we take security and reliability seriously. We have implemented comprehensive measures to ensure the utmost security and protection of our residents' details. Your data privacy and safety are our top priorities."
+    },
+
+    {
+        id: 6,
+        question: "What should I do if I encounter technical issues with the software?",
+        answer: "If you encounter any issues, please don't hesitate to contact us at support@estility.co. Simply provide us with the details of the issue, and we will work diligently to resolve it promptly. Your satisfaction is our priority."
     }
 ]
 
